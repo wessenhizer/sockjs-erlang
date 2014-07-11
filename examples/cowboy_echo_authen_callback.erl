18f4da619dbf2f93f15e501626dfb2abf54c65a2
Vuong B. Tru <trubavuong@gmail.com>
2014-07-11 08:30:02 +0700

    Add some examples for new authentication callback feature.

---
+++ examples/cowboy_echo_authen_callback.erl
@@ -0,0 +1,69 @@
+#!/usr/bin/env escript
+%%! -smp disable +A1 +K true -pa ebin -env ERL_LIBS deps -input
+-module(cowboy_echo).
+-mode(compile).
+
+-export([main/1]).
+
+%% Cowboy callbacks
+-export([init/3, handle/2, terminate/3]).
+
+
+main(_) ->
+    Port = 8081,
+    ok = application:start(xmerl),
+    ok = application:start(sockjs),
+    ok = application:start(ranch),
+    ok = application:start(crypto),
+    ok = application:start(cowlib),
+    ok = application:start(cowboy),
+
+    SockjsState = sockjs_handler:init_state(
+                    <<"/echo">>, fun authen/3, fun service_echo/3, state, []),
+
+    VhostRoutes = [{<<"/echo/[...]">>, sockjs_cowboy_handler, SockjsState},
+                   {'_', ?MODULE, []}],
+    Routes = [{'_',  VhostRoutes}], % any vhost
+    Dispatch = cowboy_router:compile(Routes),
+
+    io:format(" [*] Running at http://localhost:~p~n", [Port]),
+    cowboy:start_http(cowboy_echo_http_listener, 100,
+                      [{port, Port}],
+                      [{env, [{dispatch, Dispatch}]}]),
+    receive
+        _ -> ok
+    end.
+
+%% --------------------------------------------------------------------------
+
+init({_Any, http}, Req, []) ->
+    {ok, Req, []}.
+
+handle(Req, State) ->
+    {ok, Data} = file:read_file("./examples/echo.html"),
+    {ok, Req1} = cowboy_req:reply(200, [{<<"Content-Type">>, "text/html"}],
+                                       Data, Req),
+    {ok, Req1, State}.
+
+terminate(_Reason, _Req, _State) ->
+    ok.
+
+%% --------------------------------------------------------------------------
+
+authen(_Conn, init, State) ->
+    {ok, State};
+authen(Conn, {recv, Data}, State) ->
+    case Data of
+        <<"auth">> ->
+            Conn:send(<<"Authenticate successfully!">>),
+            success;
+        _Else ->
+            {ok, State}
+    end;
+authen(_Conn, closed, State) ->
+    {ok, State}.
+
+service_echo(_Conn, init, state)          -> {ok, state};
+service_echo(Conn, {recv, Data}, state)   -> sockjs:send(Data, Conn);
+service_echo(_Conn, {info, _Info}, state) -> {ok, state};
+service_echo(_Conn, closed, state)        -> {ok, state}.
---
+++ examples/multiplex/cowboy_multiplex_authen_callback.erl
@@ -0,0 +1,100 @@
+#!/usr/bin/env escript
+%%! -smp disable +A1 +K true -pa ebin -env ERL_LIBS deps -input
+-module(cowboy_multiplex).
+-mode(compile).
+
+-export([main/1]).
+
+%% Cowboy callbacks
+-export([init/3, handle/2, terminate/3]).
+
+main(_) ->
+    Port = 8081,
+    ok = application:start(xmerl),
+    ok = application:start(sockjs),
+    ok = application:start(ranch),
+    ok = application:start(crypto),
+    ok = application:start(cowlib),
+    ok = application:start(cowboy),
+
+    MultiplexState = sockjs_multiplex:init_state(
+                       [{"ann",  fun service_ann/3,  []},
+                        {"bob",  fun service_bob/3,  []},
+                        {"carl", fun service_carl/3, []}]),
+
+    SockjsState = sockjs_handler:init_state(
+                    <<"/multiplex">>, fun authen/3, sockjs_multiplex, MultiplexState, []),
+
+    VhostRoutes = [{<<"/multiplex/[...]">>, sockjs_cowboy_handler, SockjsState},
+                   {'_', ?MODULE, []}],
+    Routes = [{'_',  VhostRoutes}], % any vhost
+    Dispatch = cowboy_router:compile(Routes),
+
+    io:format(" [*] Running at http://localhost:~p~n", [Port]),
+    cowboy:start_http(http, 100,
+                      [{port, Port}],
+                      [{env, [{dispatch, Dispatch}]}]),
+    receive
+        _ -> ok
+    end.
+
+%% --------------------------------------------------------------------------
+
+init({_Any, http}, Req, []) ->
+    {ok, Req, []}.
+
+handle(Req, State) ->
+    {Path, Req1} = cowboy_req:path(Req),
+    {ok, Req2} = case Path of
+                     <<"/">> ->
+                         {ok, Data} = file:read_file("./examples/multiplex/index_authen_callback.html"),
+                         cowboy_req:reply(200, [{<<"Content-Type">>, "text/html"}],
+                                               Data, Req1);
+                     _ ->
+                         cowboy_req:reply(404, [],
+                                               <<"404 - Nothing here\n">>, Req1)
+                 end,
+    {ok, Req2, State}.
+
+terminate(_Reason, _Req, _State) ->
+    ok.
+
+%% --------------------------------------------------------------------------
+
+authen(_Conn, init, State) ->
+    {ok, State};
+authen(Conn, {recv, Data}, State) ->
+    case Data of
+        <<"auth">> ->
+            Conn:send(<<"Authenticate successfully!">>),
+            success;
+        _Else ->
+            {ok, State}
+    end;
+authen(_Conn, closed, State) ->
+    {ok, State}.
+
+service_ann(Conn, init, State) ->
+    sockjs:send("Ann says hi!", Conn),
+    {ok, State};
+service_ann(Conn, {recv, Data}, State) ->
+    sockjs:send(["Ann nods: ", Data], Conn),
+    {ok, State};
+service_ann(_Conn, closed, State) ->
+    {ok, State}.
+
+service_bob(Conn, init, State) ->
+    sockjs:send("Bob doesn't agree.", Conn),
+    {ok, State};
+service_bob(Conn, {recv, Data}, State) ->
+    sockjs:send(["Bob says no to: ", Data], Conn),
+    {ok, State};
+service_bob(_Conn, closed, State) ->
+    {ok, State}.
+
+service_carl(Conn, init, State) ->
+    sockjs:send("Carl says goodbye!", Conn),
+    sockjs:close(Conn),
+    {ok, State};
+service_carl(_Conn, _, State) ->
+    {ok, State}.
